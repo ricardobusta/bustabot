34f15249d8722f6dc98f7199f7073259137b0240
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2019-02-10 15:05:21 -0200

    add bot secret path

--- app.js
+++ app.js
@@ -19,9 +19,9 @@
 const express = require('express');
 
 const path = require('path');
-const bot_key = require(path.resolve( __dirname, "./botkey.js" ));
+const bot_key = require(path.resolve( __dirname, "./botkey.js" )).key;
 
-const bot_url = 'https://api.telegram.org/'+bot_key.key+'/';
+const bot_url = 'https://api.telegram.org/'+bot_key+'/';
 
 const app = express();
 
@@ -32,10 +32,10 @@ app.get('/', (req, res) => {
     .end();
 });
 
-app.get('/webhook', (req, res) => {
+app.get('/'+bot_key, (req, res) => {
   var param = req.query.param;
 
-  console.log(bot_url);
+  console.log(req.query)
 
   res
     .status(200)
@@ -48,5 +48,6 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
+  console.log('bot path '+ '/'+bot_key)
 });
 // [END gae_node_request_example]
