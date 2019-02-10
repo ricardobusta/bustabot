37139689368eef05d143af824059c79000cdbf08
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2019-02-10 18:34:07 -0200

    use json body parser (or something like that)

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-/**
- * Copyright 2017, Google, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
-// [START gae_node_request_example]
 const express = require('express');
 
 const path = require('path');
@@ -24,6 +8,8 @@ const bot_key = require(path.resolve( __dirname, "./botkey.js" )).key;
 const bot_url = 'https://api.telegram.org/'+bot_key+'/';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res
@@ -57,4 +43,3 @@ app.listen(PORT, () => {
   console.log('Press Ctrl+C to quit.');
   console.log('bot path '+ '/'+bot_key)
 });
-// [END gae_node_request_example]
