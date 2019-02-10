ffa8f007d099cfb6ebce2ea4fd7f1014740415ec
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2019-02-10 16:48:53 -0200

    add post request

--- app.js
+++ app.js
@@ -41,6 +41,15 @@ app.get('/'+bot_key, (req, res) => {
     .end();
 });
 
+app.post('/'+bot_key, (req, res) => {
+  console.log(req.body)
+
+  res
+    .status(200)
+    .send("Bot Working!")
+    .end();
+});
+
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
