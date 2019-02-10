f378f51de91bc48a2162fd33f6ab04c2cff1f4d5
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2019-02-10 16:04:47 -0200

    ops

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/'+bot_key, (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
