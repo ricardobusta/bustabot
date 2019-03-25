f63ff3c589b049c9a98250755d9d9c4d2196a63a
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2019-03-25 03:33:13 -0300

    Improve Roll Command.

--- bot_commands/roll_command.js
+++ bot_commands/roll_command.js
@@ -1,11 +1,15 @@
 const telegramCommands = require('../bot_core/telegram_commands');
 
+function RandomRange(min, max){
+    return Math.floor((Math.random()*(max-min+1)+min));
+}
+
 module.exports = {
     keys: ["roll", "dice"],
     help: "Rolls a dice.",
     execute: function(params, req){
         telegramCommands.sendMessage(
             req.message.chat.id,
-            "Rolling Dice: " + Math.floor((Math.random()*6)+1).toString());
+            "<code>Rolling Dice: " + RandomRange(1,6) + "</code>");
     }
 }
