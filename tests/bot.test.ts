9dae4dbe5f7e9d022f5869a18aa82912a8f9d11c
Ricardo Bustamante <ricardo@busta.dev>
2020-08-15 13:47:50 +0200

    fix test

--- tests/bot.test.ts
+++ tests/bot.test.ts
@@ -3,6 +3,6 @@ import { expect } from "chai";
 describe('calculate', function () {
     it('add', function () {
         let result = 5 + 3;
-        expect(result).equal(7);
+        expect(result).equal(8);
     });
 });
