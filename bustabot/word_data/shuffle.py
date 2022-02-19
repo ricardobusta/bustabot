831b0f0c066d3025975f2c6e7635645202da1f71
Ricardo Bustamante <ricardo@busta.dev>
2022-02-19 17:18:46 +0100

    fix shuffle

--- bustabot/word_data/shuffle.py
+++ bustabot/word_data/shuffle.py
@@ -1,7 +1,7 @@
 import random
-lines = open('./database_ptbr_src.txt', 'r', encoding='utf8').readlines()
+lines = open('./all_ptbr_out.txt', 'r', encoding='utf8').readlines()
 lines = list(dict.fromkeys(lines))
 for i in range(len(lines)):
-    lines[i] = lines[i].lower()
+    lines[i] = lines[i].upper()
 random.shuffle(lines)
 open('./database_ptbr_out.txt', 'w', encoding='utf8').writelines(lines)
